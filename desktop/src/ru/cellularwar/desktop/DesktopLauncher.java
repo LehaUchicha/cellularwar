3f61c7b836f8b7445827344a84c75a4a16c507e9
Alex <alerealalex@rambler.ru>
2018-04-27 00:44:22 +0500

    initCommit

---
+++ desktop/src/ru/cellularwar/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.cellularwar.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.cellularwar.CellularWar;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new CellularWar(), config);
+	}
+}
